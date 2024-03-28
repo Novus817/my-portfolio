c689368b2b8841e898e643f833e1205608ec14f9
Anthony Marrello <apm817@gmail.com>
2024-03-28 19:01:35 -0400

    trying again

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -21,7 +21,7 @@ type Props = {
 
 export default async function Home({ params }: Props) {
   const projects = await getProjects();
-  revalidatePath('/work');
+  revalidatePath('/work[slug]');
 
   return (
     <div className="container mx-auto">
