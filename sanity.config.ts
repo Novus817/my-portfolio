f94631b6c932a84de87632253e4cd6f0609b6065
Anthony Marrello <apm817@gmail.com>
2024-11-02 01:33:34 -0400

    add vision tool

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'sanity';
 import { structureTool } from 'sanity/structure';
+import { visionTool } from '@sanity/vision';
 import schemas from './sanity/schemas';
 
 const config = defineConfig({
@@ -8,7 +9,7 @@ const config = defineConfig({
   title: 'My Portfolio Website',
   apiVersion: '2023-07-09',
   basePath: '/admin',
-  plugins: [structureTool()],
+  plugins: [structureTool(), visionTool()],
   schema: { types: schemas },
 });
 
