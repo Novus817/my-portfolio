07cc3f9990198663f5863fe3ae1bb47a0a22d727
Anthony Marrello <apm817@gmail.com>
2025-04-26 01:49:08 -0400

    extract tailwind styles for cleanup in header

--- app/(site)/components/Header.tsx
+++ app/(site)/components/Header.tsx
@@ -5,21 +5,14 @@ export default async function Header() {
   const pages = await getPages();
 
   return (
-    <header className="flex items-center justify-between flex-col sm:flex-row sm:mb-5">
-      <Link
-        href="/"
-        className="mb-4 sm:mb-0 bg-gradient-to-r from-orange-800 to-orange-600 via-orange-700 bg-clip-text text-transparent text-xl font-bold"
-      >
+    <header className="header-section">
+      <Link href="/" className="logo-link">
         Anthony Marrello | Portfolio
       </Link>
 
-      <div className="flex items-center gap-5 text-orange-600">
+      <div className="main-nav">
         {pages.reverse().map((page) => (
-          <Link
-            key={page._id}
-            href={`/${page.slug}`}
-            className="border-b-4 border-transparent hover:border-orange-800 transition"
-          >
+          <Link key={page._id} href={`/${page.slug}`} className="nav-link">
             {page.title}
           </Link>
         ))}
