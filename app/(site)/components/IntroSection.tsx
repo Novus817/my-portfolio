de7e451359482c29e0ae29a766598f791e26c3b1
Anthony Marrello <apm817@gmail.com>
2025-04-26 02:35:46 -0400

    extract tailwind styles for cleanup in intro

--- app/(site)/components/IntroSection.tsx
+++ app/(site)/components/IntroSection.tsx
@@ -1,17 +1,11 @@
 export default function IntroSection() {
   return (
     <>
-      <h1 className="text-center sm:text-left mb-2 sm:mb-5 text-5xl sm:text-6xl md:text-7xl font-extrabold tracking-wide">
-        Hello, I&apos;m{' '}
-        <span className="bg-gradient-to-r from-orange-800 to-orange-600 via-orange-700 bg-clip-text text-transparent">
-          Anthony
-        </span>
-        !
+      <h1 className="heading-text">
+        Hello, I&apos;m <span className="heading-span">Anthony</span>!
       </h1>
 
-      <p className="text-lg sm:text-xl text-center sm:text-left mb-10 sm:mb-14 tracking-widest">
-        Welcome to My Portfolio.
-      </p>
+      <p className="tagline-text">Welcome to My Portfolio.</p>
     </>
   );
 }
