807cda2b55b144c1efd27ebe0eec4284aac0e59d
Anthony Marrello <apm817@gmail.com>
2025-04-26 01:56:50 -0400

    replace nav with component

--- app/(site)/components/Header.tsx
+++ app/(site)/components/Header.tsx
@@ -1,22 +1,13 @@
 import Link from 'next/link';
-import { getPages } from '@/sanity/sanity-utils';
+import Nav from './Nav';
 
 export default async function Header() {
-  const pages = await getPages();
-
   return (
     <header className="header-section">
       <Link href="/" className="logo-link">
         Anthony Marrello | Portfolio
       </Link>
-
-      <div className="main-nav">
-        {pages.reverse().map((page) => (
-          <Link key={page._id} href={`/${page.slug}`} className="nav-link">
-            {page.title}
-          </Link>
-        ))}
-      </div>
+      <Nav />
     </header>
   );
 }
