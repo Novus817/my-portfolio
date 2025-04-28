fffd101cfdce26a74a043d855439cda09baccae0
Anthony Marrello <apm817@gmail.com>
2025-04-28 04:27:13 -0400

    refactored work component content

--- app/(site)/components/WorkPage.tsx
+++ app/(site)/components/WorkPage.tsx
@@ -2,8 +2,14 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import { getProjects } from '@/sanity/sanity-utils';
+import { PortableText } from '@portabletext/react';
+import { PortableTextBlock } from 'sanity';
 
-export default async function WorkPage() {
+type Props = {
+  content: PortableTextBlock[] | undefined;
+};
+
+export default async function WorkPage({ content }: Props) {
   const projects = await getProjects();
 
   if (!projects) {
@@ -11,26 +17,31 @@ export default async function WorkPage() {
   }
 
   return (
-    <div className="work-wrap">
-      {projects.map((project) => (
-        <Link
-          href={`/work/${project.slug}`}
-          key={project._id}
-          className="project-link"
-        >
-          {project.image && (
-            <Image
-              src={project.image}
-              alt={project.name}
-              width={750}
-              height={300}
-              className="project-img"
-            />
-          )}
+    <>
+      <div className="content-section">
+        <PortableText value={content} />
+      </div>
+      <div className="work-wrap">
+        {projects.map((project) => (
+          <Link
+            href={`/work/${project.slug}`}
+            key={project._id}
+            className="project-link"
+          >
+            {project.image && (
+              <Image
+                src={project.image}
+                alt={project.name}
+                width={750}
+                height={300}
+                className="project-img"
+              />
+            )}
 
-          <div className="project-name">{project?.name}</div>
-        </Link>
-      ))}
-    </div>
+            <div className="project-name">{project?.name}</div>
+          </Link>
+        ))}
+      </div>
+    </>
   );
 }
