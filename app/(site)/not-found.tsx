61246403ed6d1afe78ecec01f635a59161ccaf5d
Anthony Marrello <apm817@gmail.com>
2025-04-28 00:08:25 -0400

    extract not-found styles for cleanup

--- app/(site)/not-found.tsx
+++ app/(site)/not-found.tsx
@@ -2,15 +2,12 @@ import Link from 'next/link';
 
 export default function Custom404() {
   return (
-    <div className="flex flex-col justify-center mx-auto my-8">
-      <h2 className="text-3xl">There was a problem.</h2>
+    <div className="not-found-wrap">
+      <h2 className="not-found-title">There was a problem.</h2>
       <p>The page you requested does not exist.</p>
       <p>
         Go back to the{' '}
-        <Link
-          href="/"
-          className="text-orange-600 border-b-4 border-transparent hover:border-orange-800 transition"
-        >
+        <Link href="/" className="homepage-link">
           Homepage
         </Link>
       </p>
