ca8a900d7251b50ee472de147b8fef3e3af90b2f
Anthony Marrello <apm817@gmail.com>
2025-04-28 04:23:52 -0400

    refactored aboutPage

--- app/(site)/components/AboutPage.tsx
+++ app/(site)/components/AboutPage.tsx
@@ -1,15 +1,26 @@
 import Image from 'next/image';
+import { PortableText } from '@portabletext/react';
+import { PortableTextBlock } from 'sanity';
 
-export default function AboutPage() {
+type Props = {
+  content: PortableTextBlock[] | undefined;
+};
+
+export default async function AboutPage({ content }: Props) {
   return (
-    <div className="about-img-wrap">
-      <Image
-        src="/abc-headshot.jpeg"
-        width={640}
-        height={360}
-        className="about-img"
-        alt="Me at work"
-      />
-    </div>
+    <>
+      <div className="about-img-wrap">
+        <Image
+          src="/abc-headshot.jpeg"
+          width={640}
+          height={360}
+          className="about-img"
+          alt="Me at work"
+        />
+      </div>
+      <div className="content-section">
+        <PortableText value={content} />
+      </div>
+    </>
   );
 }
