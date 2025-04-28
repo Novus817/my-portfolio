6df0721bd5bd22a54bf200d3620c9dd01e510afc
Anthony Marrello <apm817@gmail.com>
2025-04-28 00:56:47 -0400

    create about page component

---
+++ app/(site)/components/AboutPage.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+
+export default async function AboutPage() {
+  return (
+    <div className="about-img-wrap">
+      <Image
+        src="/abc-headshot.jpeg"
+        width={640}
+        height={360}
+        className="about-img"
+        alt="Me at work"
+      />
+    </div>
+  );
+}
