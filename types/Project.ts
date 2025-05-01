b9f0fc19f0d0e73bb8dae10d04912bcda2148eef
Anthony Marrello <apm817@gmail.com>
2025-05-01 04:34:35 -0400

    add alt type in project

--- types/Project.ts
+++ types/Project.ts
@@ -1,4 +1,4 @@
-import { PortableTextBlock } from "sanity";
+import { PortableTextBlock } from 'sanity';
 
 export type Project = {
   _id: string;
@@ -6,6 +6,7 @@ export type Project = {
   name: string;
   slug: string;
   image: string;
+  alt: string;
   url: string;
   content: PortableTextBlock[];
-}
+};
