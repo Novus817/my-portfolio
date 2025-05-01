3b3ad2988f0ca054283396f58b60e7fdcbfa7380
Anthony Marrello <apm817@gmail.com>
2025-05-01 04:42:47 -0400

    add props for metadata and content - reduce data fetching

--- app/(site)/[slug]/page.tsx
+++ app/(site)/[slug]/page.tsx
@@ -1,19 +1,26 @@
-import { getPage } from '@/sanity/sanity-utils';
+import { getPage, getProjects } from '@/sanity/sanity-utils';
 import { notFound } from 'next/navigation';
 import AboutPage from '../components/AboutPage';
 import ContactPage from '../components/ContactPage';
 import WorkPage from '../components/WorkPage';
 
 type Props = {
-  params: { slug: string; title: string };
+  params: { slug: string; title?: string };
 };
 
 export async function generateMetadata({ params }: Props) {
   const page = await getPage(params.slug);
 
+  if (!page) {
+    return {
+      title: 'Anthony Marrello Portfolio | Not Found',
+      description: 'Page not found',
+    };
+  }
+
   return {
-    title: `Anthony Marrello | Portfolio | ${page?.title}`,
-    description: `My portfolio page for ${page?.title}`,
+    title: `Anthony Marrello | Portfolio | ${page.title}`,
+    description: `My portfolio page for ${page.title}`,
   };
 }
 
@@ -24,15 +31,20 @@ export default async function Page({ params }: Props) {
     return notFound();
   }
 
+  // Fetch projects only if the slug is 'work'
+  const projects = page.slug === 'work' ? await getProjects() : null;
+
   return (
     <div>
-      <h1 className="page-title">{page?.title}</h1>
+      <h1 className="page-title">{page.title}</h1>
 
-      {page?.slug === 'about' && <AboutPage content={page?.content} />}
+      {page?.slug === 'about' && <AboutPage content={page.content} />}
 
-      {page?.slug === 'contact' && <ContactPage content={page?.content} />}
+      {page?.slug === 'contact' && <ContactPage content={page.content} />}
 
-      {page?.slug === 'work' && <WorkPage content={page?.content} />}
+      {page?.slug === 'work' && (
+        <WorkPage content={page.content} projects={projects} />
+      )}
     </div>
   );
 }
